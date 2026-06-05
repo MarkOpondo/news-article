4af955b3564393d854dd095adc98b136d101b200
Mark Opondo <markopondoi.t@gmail.com>
2026-06-05 06:43:15 +0300

    specific word count function works

--- pythonAssessment.py
+++ pythonAssessment.py
@@ -1,9 +1,16 @@
 import re
+from collections import Counter
 
 with open('NewsArticleforPythonAssessment.txt', 'r', encoding='utf-8') as file:
     article = file.read()
 
-print(article)
 
-def count_specific_word():
-    None
+def count_specific_word(target_word, article):
+    all_words = re.findall(r"\b\w+\b", article.lower())
+    word_count = Counter(all_words)
+    target_count = word_count[target_word.lower()]
+
+    print(target_count)
+    
+
+count_specific_word('the', article)
