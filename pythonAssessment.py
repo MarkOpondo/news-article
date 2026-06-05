b54822436ebefa9919ca42991b3da74b10c8489b
Mark Opondo <markopondoi.t@gmail.com>
2026-06-05 08:14:34 +0300

    paragraph count function works

--- pythonAssessment.py
+++ pythonAssessment.py
@@ -3,14 +3,11 @@ from pathlib import Path
 from collections import Counter
 
 article = Path("NewsArticleforPythonAssessment.txt").read_text(encoding="utf-8")
-if not article:
-    None
-else:
-    all_words = re.findall(r"\b\w+\b", article.lower())
-    word_count = Counter(all_words)
 
 
 def count_specific_word(target_word, article):
+    all_words = re.findall(r"\b\w+\b", article.lower())
+    word_count = Counter(all_words)
     target_count = word_count[target_word.lower()]
 
     if not target_count:
@@ -20,6 +17,8 @@ def count_specific_word(target_word, article):
     
 
 def identify_most_common_word(article):
+    all_words = re.findall(r"\b\w+\b", article.lower())
+    word_count = Counter(all_words)
     most_common_word, frequency = word_count.most_common(1)[0]
 
     if not most_common_word:
@@ -29,6 +28,7 @@ def identify_most_common_word(article):
 
 
 def calculate_average_word_length(article):
+    all_words = re.findall(r"\b\w+\b", article.lower())
     total_words = len(all_words)
     total_letters = sum(len(word) for word in all_words)
 
